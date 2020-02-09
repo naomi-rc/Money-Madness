81dff6fa514e34905e385bc4e27bfc9f544b4a70
naomi-rc <naomi.cat182128@gmail.com>
2020-02-08 23:04:51 -0500

    add scipts to DollarBills and SubmitButton

---
+++ Assets/DollarBill.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DollarBill : MonoBehaviour
+{
+    private GameObject AmountValue;
+    private static double amount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AmountValue = GameObject.Find("AmountValue");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+  
+    private void OnMouseDown()
+    {
+        var TextMesh = AmountValue.GetComponent("TextMesh");
+        switch (name)
+        {
+            case "DollarBill20": amount += 20; break;
+            case "DollarBill10": amount += 10; break;
+            case "DollarBill5": amount += 5; break;
+            case "DollarBill1": amount += 1; break;
+        }
+        if (TextMesh != null)
+        {
+            ((TextMesh)TextMesh).text = "$" + amount;
+           
+        }
+
+    }
+
+    public static double GetAmount()
+    {
+        return amount;
+    }
+}
---
+++ Assets/SubmitButton.cs
@@ -0,0 +1,34 @@
+﻿//using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SubmitButton : MonoBehaviour
+{
+    private double Cost;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cost = Random.Range(0, 100);        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        ValidateSubmission();
+    }
+
+    private void ValidateSubmission()
+    {
+        if(DollarBill.GetAmount() == Cost)
+        {
+            print("You got it! Congrats!");
+        }
+    }
+}
